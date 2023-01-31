5ae281ead09f91cdd13c70f2af4556dc85a2225e
sachinrao2811 <sachinrao9450@gmail.com>
2023-01-31 17:34:34 +0530

    created get api

--- controller/products.js
+++ controller/products.js
@@ -1,11 +1,18 @@
 const Product = require("../models/product");
 
 const getAllProduct = async (req, resp)=>{
-    const MyProduct =await Product.find({});
+    const QueryObject = {}
+    const { company } =req.query;
+    if(company){
+        QueryObject.company= company;
+    }
+
+    const MyProduct =await Product.find(QueryObject);
     resp.status(200).json({MyProduct});
 }
 const getAllProductTesting = async (req, resp)=>{
-    resp.status(200).json({setMAsg: " Hello i am testing"});
+    const MyProduct = await Product.find({});
+    resp.status(200).json({MyProduct});
 }
 
 module.exports = {getAllProduct, getAllProductTesting};
